1c37b26e55b9fdcbc78f773c1013338f73a1bf12
WYW <2637405168@qq.com>
2018-04-02 20:45:04 +0800

    一点小结

--- WWCollection/WWCollection/ReadMe/ReadMe.h
+++ WWCollection/WWCollection/ReadMe/ReadMe.h
@@ -21,6 +21,19 @@
  在这里看到了答案：https://stackoverflow.com/questions/8883818/unterminated-conditional-directive-in-xcode
  其实就是对应的#ifdef 和#endif不配对 导致的
  
+ 2018-03-29的时候因为在命名的时候一个WYWCollectionView和一个WYWCollectionViewCell的命名的冲突的问题相关的引用已经被删除了暂时
+ 
+ 2018-03-31 CocoaPods 1.5.0.beta.1 is available.
+ To update use: `sudo gem install cocoapods --pre`
+ 使用这个命令更新了Cocoapods
+ 起因是pod search 不到MJRefresh 的3.15.4 search 到3.15.3 不过后来用了beta版的Cocoapods 仍然只能搜索到3.15.3
+ 
+ 2018-04-02 有的时候设置合适的约束 也会给程序意想不到的效果 设置一个视图的上左下右 和 父视图都一样跟单纯的设置frame的方式是不同的 尤其在操作可以滚动的时候的时候这一点会有不一样的收获
+ 轮播图的SDCycleScrollView还是挺好用的 设置挺简单的 自己在写相关内容的时候如果是全屏宽度的CollectionViewCell的时候总是会有中间的间隙去不掉 还需要看看究竟
+ 基类的合适使用有利于代码的复用 尤其像背景色 背景图 导航栏的部分的某些图标
+ 
+ 
+ 
  
  
  */
