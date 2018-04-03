ab4eb02b0fb5702655745db48e6b4a070106db83
WYW <2637405168@qq.com>
2018-04-03 20:06:30 +0800

    UIScrollView上放置多个UITableView，并且顶部公用同一个头视图的问题   如果UITableView是同类型的还需要考虑的一个问题是重用UITableView

--- WWCollection/WWCollection/ReadMe/ReadMe.h
+++ WWCollection/WWCollection/ReadMe/ReadMe.h
@@ -32,6 +32,23 @@
  轮播图的SDCycleScrollView还是挺好用的 设置挺简单的 自己在写相关内容的时候如果是全屏宽度的CollectionViewCell的时候总是会有中间的间隙去不掉 还需要看看究竟
  基类的合适使用有利于代码的复用 尤其像背景色 背景图 导航栏的部分的某些图标
  
+ 2018-04-02 一点点收获是 如果对于一个 mainScrollView 承载着多个TableView
+ 
+|-                headView                    -|
+ |TableView|  |TableView|   |TableView|   |TableView|
+ 
+ 解决方案：
+    * 可以考虑一个mainScrollView 承载着多个UITableView 可以以添加子控制器的方式添加
+ 给每个控制器设置代理 来传递到 TableView的滚动
+    * 多个UITableView每个都设置TableHeaderView 而且高度是顶部的headView的高度 不过只是留着做占位使用而已
+    * 顶部的headView选择最后添加 添加的时候添加到self.view上
+    * 在各个TableView滚动的时候如果想让headView也滚动 则在相关的滚动的代理方法中根据滚动的偏移量来改变顶部的headView
+    * 在滚动某一个UITableView的时候
+    ！！注意适当更改其他的UITableView的偏移！！
+        这样才可以适当避免闪动 其实自己就卡在了这一步过
+ 参考地址：https://github.com/liuzhongning/NNJaneBookView
+ 
+ 
  
  
  
